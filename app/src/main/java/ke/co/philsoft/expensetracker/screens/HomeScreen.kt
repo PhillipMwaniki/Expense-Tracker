6c5c825766b88afdf5cfb381194c1550a35ce623
Phillip Mwaniki Nzuli <codezeroprivate@gmail.com>
2024-12-31 10:54:31 +0300

    Initial commit

---
+++ app/src/main/java/ke/co/philsoft/expensetracker/screens/HomeScreen.kt
@@ -0,0 +1,21 @@
+package ke.co.philsoft.expensetracker.screens
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun HomeScreen() {
+    Surface(modifier = Modifier.fillMaxSize()) {
+
+    }
+}
+
+
+@Composable
+@Preview(showBackground = true)
+fun PreviewHomeScreen() {
+    HomeScreen()
+}
