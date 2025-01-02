9be0a96aa0299399c9a9dbba46fafc7d542fcaae
Phillip Mwaniki Nzuli <codezeroprivate@gmail.com>
2025-01-02 11:53:37 +0300

    Added expense box and transaction list

--- app/src/main/java/ke/co/philsoft/expensetracker/screens/HomeScreen.kt
+++ app/src/main/java/ke/co/philsoft/expensetracker/screens/HomeScreen.kt
@@ -1,15 +1,234 @@
 package ke.co.philsoft.expensetracker.screens
 
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+
+import ke.co.philsoft.expensetracker.R
+import ke.co.philsoft.expensetracker.ui.theme.Zinc
+import kotlinx.coroutines.NonDisposableHandle.parent
 
 @Composable
 fun HomeScreen() {
     Surface(modifier = Modifier.fillMaxSize()) {
+        ConstraintLayout(modifier = Modifier.fillMaxSize()){
+            val (nameRow, list, card, topBar) = createRefs()
+            Image(
+                painter = painterResource(R.drawable.ic_topbar),
+                contentDescription = null,
+                modifier = Modifier.constrainAs(topBar) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 64.dp, start = 16.dp, end = 16.dp)
+                    .constrainAs(nameRow) {
+                        top.linkTo(parent.top)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    }
+            ) {
+                Column(modifier = Modifier.align(Alignment.CenterStart)) {
+                    Text(text = "Hello, Phillip", fontSize = 16.sp, color = Color.White)
+                    Text(
+                        text = "Expense Tracker",
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+                    )
+                }
 
+                Image(
+                    painter = painterResource(R.drawable.ic_notification),
+                    contentDescription = null,
+                    modifier = Modifier.align(Alignment.CenterEnd)
+                )
+            }
+
+            CardItem(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp, start = 16.dp, end = 16.dp)
+                    .constrainAs(card) {
+                        top.linkTo(nameRow.bottom)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    }
+            )
+            TransactionList(modifier = Modifier
+                .fillMaxWidth()
+                .constrainAs(list) {
+                    top.linkTo(card.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                    bottom.linkTo(parent.bottom)
+                    height = Dimension.fillToConstraints
+                })
+        }
+    }
+}
+
+@Composable
+fun CardItem(modifier: Modifier) {
+    Column(modifier = modifier
+        .padding(16.dp)
+        .fillMaxWidth()
+        .height(200.dp)
+        .clip(RoundedCornerShape(16.dp))
+        .background(Zinc)
+        .padding(16.dp)
+    ){
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .weight(1f)) {
+            Column(modifier = Modifier.align(Alignment.CenterStart)) {
+                Text(text = "Total Balance", fontSize = 16.sp, color = Color.White)
+                Text(text = "£100", fontSize = 24.sp, color = Color.White)
+            }
+
+            Image(
+                painter = painterResource(R.drawable.dots_menu),
+                contentDescription = null,
+                modifier = Modifier.align(Alignment.CenterEnd)
+            )
+
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            CardRowItem(title = "Income", amount = "£ 7,000", image = R.drawable.ic_income)
+            CardRowItem(title = "Expenses", amount = "£ 1,000", image = R.drawable.ic_expense)
+        }
+    }
+}
+
+@Composable
+fun CardRowItem(title: String, amount: String, image: Int) {
+    Column {
+        Row {
+            Image(
+                painter = painterResource(image),
+                contentDescription = null
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+            Text(text = title, fontSize = 16.sp, color = Color.White)
+        }
+        Text(text = amount, fontSize = 20.sp, color = Color.White)
+    }
+}
+
+
+@Composable
+fun TransactionList(modifier: Modifier) {
+    Column(modifier = modifier.padding(horizontal = 16.dp)) {
+        Box(modifier = modifier.fillMaxWidth()) {
+            Text(text = "Recent Transactions", fontSize = 20.sp)
+            Text(
+                text = "See All",
+                fontSize = 16.sp,
+                modifier = Modifier.align(Alignment.CenterEnd)
+            )
+        }
+
+        TransactionItem(
+            title = "Income",
+            amount = "- £ 20",
+            date = "Today",
+            icon = R.drawable.ic_netflix,
+            color = Color.Red
+        )
+
+        TransactionItem(
+            title = "Upwork",
+            amount = "+ £ 2000",
+            date = "Today",
+            icon = R.drawable.ic_upwork,
+            color = Color.Green
+        )
+
+        TransactionItem(
+            title = "Paypal",
+            amount = "- £ 780",
+            date = "Today",
+            icon = R.drawable.ic_paypal,
+            color = Color.Green
+        )
+
+        TransactionItem(
+            title = "Starbucks",
+            amount = "- £ 15",
+            date = "Today",
+            icon = R.drawable.ic_starbucks,
+            color = Color.Red
+        )
+
+        TransactionItem(
+            title = "Youtube",
+            amount = "- £ 18",
+            date = "Today",
+            icon = R.drawable.ic_youtube,
+            color = Color.Red
+        )
+    }
+}
+
+@Composable
+fun TransactionItem(title: String, amount: String, date: String, icon: Int, color: Color) {
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .padding(vertical = 8.dp)) {
+        Row {
+            Image(
+                painter = painterResource(icon),
+                contentDescription = null,
+                modifier = Modifier.size(50.dp)
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = title, fontSize = 16.sp)
+                Text(text = date, fontSize = 12.sp)
+            }
+        }
+        Text(
+            text = amount,
+            fontSize = 20.sp,
+            modifier = Modifier.align(Alignment.CenterEnd),
+            color = color
+        )
     }
 }
 
